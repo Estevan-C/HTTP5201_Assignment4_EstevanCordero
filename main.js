64f099cb636ca716433107924119dcd5af036c5f
Estevan Cordero <estevancor@hotmail.com>
2021-08-08 13:57:21 -0400

    started git

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.scss'
+
+document.querySelector('.links').innerHTML = `
+  <ul>
+    <li><a href="index.html">Home Page</a></li>
+    <li><a href="quality_assurance.html">Quality Assurance Page</a></li>
+    <li><a href="security.html">Security Page</a></li>
+    <li><a href="bad_example.html">Bad Example Page</a></li>
+  </ul>
+`

55718d37d7670dc315b406b4d0f139e0b7b14aa8
Estevan Cordero <estevancor@hotmail.com>
2021-08-10 10:43:56 -0400

    added comments to main.js

--- main.js
+++ main.js
@@ -7,4 +7,8 @@ document.querySelector('.links').innerHTML = `
     <li><a href="security.html">Security Page</a></li>
     <li><a href="bad_example.html">Bad Example Page</a></li>
   </ul>
-`
+`; 
+// Seperate with ;
+// You can make addition query selector in here for class or ids.
+// use npm run dev if you want to see changes, 
+// npm run build && npm run serve will build and run it, and show what is like in a live server.
